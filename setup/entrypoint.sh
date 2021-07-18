d0d9393c894711c7b70b06e378eac6ff8c5256c2
radioActive DROID <me@godwin.dev>
2021-07-18 13:40:59 +0100

    Added new env PUBLIC_URL

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -16,6 +16,9 @@ sed -i 's/808080/'"$SETUP_COLOR"'/g' /opt/traccar/web/assets/mod.css
 ## Set for address search
 sed -i 's#https://geocoder.bhn.ng#'"$GEOCODE_LINK"'#g' /opt/traccar/web/assets/address.ol.js
 
+## Set public url for web
+sed -i 's#$webUrl#'"$PUBLIC_URL"'#g' /opt/traccar/templates/full/passwordReset.vm
+
 ##Set for play store link
 sed -i 's#PLAY_LINK#'"$PLAY_LINK"'#g' /opt/traccar/web/release.html
 sed -i 's#PLAY_LINK#'"$PLAY_LINK"'#g' /opt/traccar/web/debug.html
